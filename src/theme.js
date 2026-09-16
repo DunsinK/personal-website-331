e53c7b6206bb569503180e22777cd557f074c137
DunsinK <133840905+DunsinK@users.noreply.github.com>
2026-09-16 16:06:45 -0500

    fix toggle button script

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,17 @@
 function toggleStyleSheet(){
+    //localStorage.setItem("theme","./styles/style1.css")
     let current = localStorage.getItem("theme");
-    localStorage.setItem("theme","./styles/style1.css")
+    
     console.log(current)
     if (current == "./styles/style1.css"){
-        let next = "./styes/style2.css"
+        current = "./styles/style2.css"
     }
     else{
-        let next = "style1.css"
+        current = "./styles/style1.css"
     }
-    localStorage.setItem("theme", current)
 
-    document.getElementById("stylesheet").setAttribute("href", "next");
+    localStorage.setItem("theme", current)
+    document.getElementById("stylesheet").setAttribute("href", current);
 
         
 }
